a55ef4f993cd8b454b98fa3981df7d53f87cfdbc
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-05-28 20:38:05 +0530

    Third Program

---
+++ Assignment3/A3.php
@@ -0,0 +1,16 @@
+<?php
+$bookInfo = new SimpleXMLElement("<BookInfo/>");
+$book=$bookInfo ->addChild("book");
+$book->addChild("bookno","1");
+$book->addChild("bookname","java");
+$book->addChild("price","250");
+$book->addChild("year","2006");
+
+$book=$bookInfo ->addChild("book");
+$book->addChild("bookno","2");
+$book->addChild("bookname","C");
+$book->addChild("price","500");
+$book->addChild("year","1971");
+
+$bookInfo -> asXML("A3.xml");
+?>
