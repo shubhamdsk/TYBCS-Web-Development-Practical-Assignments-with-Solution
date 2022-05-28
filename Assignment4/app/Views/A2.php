4e21df73ed0bc6962b60b4dfc978866cb4bcad12
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-05-28 23:13:13 +0530

    codeigniter assignment Set A only

---
+++ Assignment4/app/Views/A2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+
+const number = parseInt(prompt("Enter a number: "));
+
+if (number > 0) {
+    alert("The number is positive");
+}
+
+else {
+    alert("The number is negative");
+}
+    </script>
+
+</body>
+</html>
